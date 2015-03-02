5b959edbedc90dc7c9ba3ef7167f64245bab15ed
Volkov Nikolay <gitkv@ya.ru>
2015-03-02 09:46:39 +0500

    add event keyUp CTRL

--- js/background.js
+++ js/background.js
@@ -1,21 +1,23 @@
 (function(){
-	if($('.learn-result-table.data-table').length>0) {
-		$('.errortext').addClass('notetext').html('Тест сдан').removeClass('errortext').css('font-weight', 'bold');
-		var countAnswer = 0;
-		var maxCountAnswer = 0;
-		$('.learn-result-table.data-table tr').each(function () {
-			if ($(this).find('th').html() == 'Количество вопросов') {
-				countAnswer = $(this).find('td').html();
-			}
-			if ($(this).find('th').html() == 'Количество правильных ответов') {
-				$(this).find('td').html(countAnswer);
-			}
-			if ($(this).find('th').html() == 'Максимальное количество баллов') {
-				maxCountAnswer = $(this).find('td').html();
-			}
-			if ($(this).find('th').html() == 'Количество набранных баллов') {
-				$(this).find('td').html(maxCountAnswer + ' (100%)');
-			}
-		});
-	}
+	$(document).on("keyup", function (event) {
+		if($('.learn-result-table.data-table').length>0 && event.keyCode == 17) {
+			$('.errortext').addClass('notetext').html('Тест сдан').removeClass('errortext').css('font-weight', 'bold');
+			var countAnswer = 0;
+			var maxCountAnswer = 0;
+			$('.learn-result-table.data-table tr').each(function () {
+				if ($(this).find('th').html() == 'Количество вопросов') {
+					countAnswer = $(this).find('td').html();
+				}
+				if ($(this).find('th').html() == 'Количество правильных ответов') {
+					$(this).find('td').html(countAnswer);
+				}
+				if ($(this).find('th').html() == 'Максимальное количество баллов') {
+					maxCountAnswer = $(this).find('td').html();
+				}
+				if ($(this).find('th').html() == 'Количество набранных баллов') {
+					$(this).find('td').html(maxCountAnswer + ' (100%)');
+				}
+			});
+		}
+	});
 })();
